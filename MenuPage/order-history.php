484adbf1885fa61cabc7d138931b216587e2f9c6
Cousigames <laurent.cousineau@gmail.com>
2020-12-03 20:00:57 -0500

    New additions

--- MenuPage/order-history.php
+++ MenuPage/order-history.php
@@ -69,11 +69,61 @@
         
     {
     
-        $array = $_POST["menu_item"];
+        $array_price = array ( 1 => "5.00", 2 => "4.00", 3 => "12.00", 4 => "7.00", 5 => "5.00", 6=> "5.00", 7=> "8.00", 8=> "5.50",
+                            9=> "7.00", 10=> "3.00",    11=> "36.00", 12 => "34.00", 13=> "16.00", 14 => "18.00",
+            15=> "24.00", 16=> "24.00", 17=> "22.00", 18 => "12.00",
+            19=> "7.50", 20=> "9.00", 21 => "8.50", 22=> "8.00",
+            23=> "6.50", 24=> "16.00");
+        
+        $array_name = array ( 1 => "Miso Soup", 2 => "Edamame", 3 => "Salad of the day", 4 => "Brussel Sprouts", 5 => "Regular Tofu", 6=> "Spicy  Tofu", 7=> "Ikura  Tofu", 8=> "Syrup  Tofu",
+                            9=> "Homemade Organic Natto", 10=> "Japanese Premium Rice",    11=> "Chirashi (+ Japanese Uni) ", 12 => "Maguro Don", 13=> "Otto Ramen", 14 => "Mushroom Mazemen",
+            15=> "Duck Mazemen", 16=> "Uni Mazemen", 17=> "Spicy Ikura Mazemen", 18 => "Karaage ",
+            19=> "Ketchup ", 20=> "Yuzu Mayo", 21 => "Sesame Dressing", 22=> "Otto Aragoshi Ponzu",
+            23=> "Soya Sauce for Sashimi", 24=> "Yuzukoshu 100G (Spicy Yuzu Paste)");
+        
+        
+        
+        
+        
+        echo "Hello<br>";
+        
+        $array = array();
+        
+        $array = $_POST['menu_item'];
+        
+        
+        //print_r ($array);
+        
+        $sum = 0;
+        
+        echo "<br>You ordered:<br><br>";
+        
+        foreach($array as $key=> $value)
+            
+        {
+           
+        echo $array_name[(int)$value] . " for " . $array_price[(int)$value] . "$.<br>";
+        
+            
+            
+           $sum += $array_price[(int)$value]; 
+            
+            
+        }
+        
+        echo "<br>Total is " . number_format($sum,2) . " dollars.";
+        
+        
+        
+        
+        
+        
     
     }
             
-        ?>    
+        ?> 
+            
+            <p><a href="menu-order.html">Go Back</a></p>
             
             
         </div>
